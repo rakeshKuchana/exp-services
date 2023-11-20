5436528a63aa7e57b241a75dc709405e66fdeb29
rakesh.kuchana <rakeshkuchana@hotmail.com>
2023-11-20 22:10:59 +0530

    Initial Setup

---
+++ src/main/java/com/exp/services/ExpServicesApplication.java
@@ -0,0 +1,13 @@
+package com.exp.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp/services/ExpServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
